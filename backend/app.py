10eefdd081532943d6438f1f2333dbbcfac94e0a
Vaibhav Nanoti <45284493+vabsnanoti@users.noreply.github.com>
2026-05-23 22:15:04 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+from risk_engine import calculate_risk
+from notification import send_email
+
+app = FastAPI(title="ChangeGuard AI")
+
+
+class ChangeRequest(BaseModel):
+    change_id: str
+    service: str
+    environment: str
+    deployment_time: str
+    changes: list
+
+
+@app.get("/")
+def health():
+    return {
+        "status": "ChangeGuard AI Running"
+    }
+
+
+@app.post("/analyze-change")
+def analyze_change(change: ChangeRequest):
+
+    result = calculate_risk(change.dict())
+
+    send_email(
+        change.change_id,
+        result["risk_level"],
+        result["recommendation"]
+    )
+
+    return {
+        "change_id": change.change_id,
+        "analysis": result
+    }
