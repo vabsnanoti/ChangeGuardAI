7eb285c2418520d5ce5de049fac40eab9dc09741
Vaibhav Nanoti <45284493+vabsnanoti@users.noreply.github.com>
2026-05-23 22:29:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import Navbar from './components/Navbar';
 import RiskCard from './components/RiskCard';
 import BlastRadius from './components/BlastRadius';
 import DependencyMap from './components/DependencyMap';
 import CABApproval from './components/CABApproval';
 import ExecutiveSummary from './components/ExecutiveSummary';
 
+import RiskFactors from './components/RiskFactors';
+import IncidentHistory from './components/IncidentHistory';
+import DeploymentTimeline from './components/DeploymentTimeline';
+import OpenAlerts from './components/OpenAlerts';
+
 import './styles.css';
+
 
 function App() {
 
@@ -16,31 +23,59 @@ function App() {
   useEffect(() => {
 
     axios.post('http://localhost:8000/analyze-change', {
+
       change_id: 'CHG001245',
+
       service: 'payment-service',
+
       environment: 'production',
+
       deployment_time: 'Friday 20:00',
+
       changes: [
         'database migration',
         'new payment API'
       ]
+
     })
     .then(response => {
+
       setData(response.data);
+
+    })
+    .catch(error => {
+
+      console.error(error);
+
     });
 
   }, []);
 
-  if (!data) return <h1>Loading...</h1>
+
+  if (!data) {
+
+    return <h1>Loading ChangeGuard AI Dashboard...</h1>;
+
+  }
+
 
   return (
+
     <div className="container">
 
-      <h1>ChangeGuard AI Dashboard</h1>
+      <Navbar />
 
       <ExecutiveSummary analysis={data.analysis} />
 
       <RiskCard analysis={data.analysis} />
+
+      <RiskFactors analysis={data.analysis} />
+
+      <OpenAlerts />
+
+      <IncidentHistory />
+
+      <DeploymentTimeline />
 
       <BlastRadius />
 
@@ -49,6 +84,7 @@ function App() {
       <CABApproval analysis={data.analysis} />
 
     </div>
+
   );
 }
 
