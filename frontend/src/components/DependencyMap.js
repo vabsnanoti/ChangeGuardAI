dd16f3184fe4c2444b8d9a8cf85ed7dad978eb4e
Vaibhav Nanoti <45284493+vabsnanoti@users.noreply.github.com>
2026-05-23 22:18:17 +0530

    Create DependencyMap.js

---
+++ frontend/src/components/DependencyMap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function DependencyMap() {
+
+  return (
+    <div className="card">
+
+      <h2>Dependency Map</h2>
+
+      <img
+        src="https://miro.medium.com/v2/resize:fit:1200/1*P6m0gS4kQ2XjZw5ZK4mNlg.png"
+        alt="dependency-map"
+        width="700"
+      />
+
+    </div>
+  );
+}
+
+export default DependencyMap;
