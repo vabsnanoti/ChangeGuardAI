8bd6dc9b4be2f82d1b14b5e6132daffe80fa85f8
Vaibhav Nanoti <45284493+vabsnanoti@users.noreply.github.com>
2026-05-23 23:10:09 +0530

    Update CABApproval.js

--- frontend/src/components/CABApproval.js
+++ frontend/src/components/CABApproval.js
@@ -1,18 +1,51 @@
 import React from 'react';
 
+
 function CABApproval({ analysis }) {
 
+  const approve =
+    analysis.risk_level === 'LOW';
+
   return (
+
     <div className="card">
 
       <h2>CAB Approval Decision</h2>
 
       {
-        analysis.risk_level === 'HIGH'
-        ?
-        <button className="reject">Reject Deployment</button>
-        :
-        <button className="approve">Approve Deployment</button>
+
+        approve ? (
+
+          <button
+            style={{
+              background: 'green',
+              color: 'white',
+              padding: '12px',
+              border: 'none',
+              borderRadius: '8px',
+              fontWeight: 'bold'
+            }}
+          >
+            APPROVE DEPLOYMENT
+          </button>
+
+        ) : (
+
+          <button
+            style={{
+              background: 'red',
+              color: 'white',
+              padding: '12px',
+              border: 'none',
+              borderRadius: '8px',
+              fontWeight: 'bold'
+            }}
+          >
+            REJECT DEPLOYMENT
+          </button>
+
+        )
+
       }
 
     </div>
