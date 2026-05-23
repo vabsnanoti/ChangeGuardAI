8a349f6dd81f09d81c28d269bb3ec0c945241ea6
Vaibhav Nanoti <45284493+vabsnanoti@users.noreply.github.com>
2026-05-23 22:24:55 +0530

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Navbar() {
+
+  return (
+    <div
+      style={{
+        background: '#111827',
+        color: 'white',
+        padding: '20px',
+        marginBottom: '20px'
+      }}
+    >
+
+      <h1>ChangeGuard AI</h1>
+
+      <p>
+        AI-Powered Deployment Risk Intelligence Platform
+      </p>
+
+    </div>
+  );
+}
+
+export default Navbar;
