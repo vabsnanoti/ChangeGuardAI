20bdb76388e44a4217a9c64761236205c75c815b
Vaibhav Nanoti <45284493+vabsnanoti@users.noreply.github.com>
2026-05-23 23:05:42 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from risk_engine import calculate_risk
 from notification import send_email
 
+
 app = FastAPI(title="ChangeGuard AI")
 
 
+# ENABLE CORS
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 class ChangeRequest(BaseModel):
+
     change_id: str
     service: str
     environment: str
@@ -17,6 +31,7 @@ class ChangeRequest(BaseModel):
 
 @app.get("/")
 def health():
+
     return {
         "status": "ChangeGuard AI Running"
     }
@@ -27,11 +42,17 @@ def analyze_change(change: ChangeRequest):
 
     result = calculate_risk(change.dict())
 
-    send_email(
-        change.change_id,
-        result["risk_level"],
-        result["recommendation"]
-    )
+    try:
+
+        send_email(
+            change.change_id,
+            result["risk_level"],
+            result["recommendation"]
+        )
+
+    except Exception as e:
+
+        print(f"Email failed: {e}")
 
     return {
         "change_id": change.change_id,
